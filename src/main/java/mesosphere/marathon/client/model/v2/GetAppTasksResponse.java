0c6089eb11fcc501ec4ee6eba6e31ca12bcbe5cf
Mohit Soni <mohitsoni1989@gmail.com>
2014-02-19 10:29:13 -0800

    Added model objects for tasks and app tasks

---
+++ src/main/java/mesosphere/marathon/client/model/v2/GetAppTasksResponse.java
@@ -0,0 +1,24 @@
+package mesosphere.marathon.client.model.v2;
+
+import java.util.Collection;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class GetAppTasksResponse {
+	private Collection<Task> tasks;
+
+	public Collection<Task> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(Collection<Task> tasks) {
+		this.tasks = tasks;
+	}
+
+	@Override
+	public String toString() {
+		Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+		return gson.toJson(this);
+	}
+}
