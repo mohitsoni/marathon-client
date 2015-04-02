4343c201147376f6f774df1eadb8c2573d88dd7d
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-04-02 16:15:29 -0700

    Added support for privileged & parameters fields

--- src/main/java/mesosphere/marathon/client/model/v2/Docker.java
+++ src/main/java/mesosphere/marathon/client/model/v2/Docker.java
@@ -1,6 +1,7 @@
 package mesosphere.marathon.client.model.v2;
 
 import java.util.Collection;
+import java.util.Map;
 
 import mesosphere.marathon.client.utils.ModelUtils;
 
@@ -8,6 +9,8 @@ public class Docker {
 	private String image;
 	private String network;
 	private Collection<Port> portMappings;
+	private Map<String, String> parameters;
+	private boolean privileged;
 
 	public String getImage() {
 		return image;
@@ -32,7 +35,23 @@ public class Docker {
 	public void setPortMappings(Collection<Port> portMappings) {
 		this.portMappings = portMappings;
 	}
-	
+
+	public boolean isPrivileged() {
+		return privileged;
+	}
+
+	public void setPrivileged(boolean privileged) {
+		this.privileged = privileged;
+	}
+
+	public Map<String, String> getParameters() {
+		return parameters;
+	}
+
+	public void setParameters(Map<String, String> parameters) {
+		this.parameters = parameters;
+	}
+
 	@Override
 	public String toString() {
 		return ModelUtils.toString(this);
